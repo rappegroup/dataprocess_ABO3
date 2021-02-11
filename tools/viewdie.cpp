fe4333464a2a9763f2076e6c08a5132902ffafcf
zhang19960128 <zjh199601283492@gmail.com>
2021-02-11 15:03:36 -0500

    chnage

--- tools/viewdie.cpp
+++ tools/viewdie.cpp
@@ -3,11 +3,29 @@
 #include <iostream>
 int main(){
   FILE* fp;
-  fp=fopen("local_die.bin","rb");
+  fp=fopen("local_polar.bin","rb");
   double localdie[3]={0.0,0.0,0.0};
-  int cell=20;
+  int cell=10;
+  int frame=20000/200;
+  double** sumlocal=new double* [cell*cell*cell];
   for(size_t i=0;i<cell*cell*cell;i++){
-  fread(localdie,sizeof(double),3,fp);
-  std::cout<<localdie[0]<<" "<<localdie[1]<<" "<<localdie[2]<<std::endl;
+    sumlocal[i]=new double [3];
+    for(size_t j=0;j<3;j++){
+      sumlocal[i][j]=0.0;
+    }
+  }
+  for(size_t t=0;t<frame;t++){
+    for(size_t i=0;i<cell*cell*cell;i++){
+      fread(localdie,sizeof(double),3,fp);
+      for(size_t j=0;j<3;j++){
+        sumlocal[i][j]=sumlocal[i][j]+localdie[j];
+      }
+    }
+  }
+  for(size_t i=0;i<cell*cell*cell;i++){
+    for(size_t j=0;j<3;j++){
+      sumlocal[i][j]=sumlocal[i][j]/frame;
+    }
+    std::cout<<sumlocal[i][0]<<" "<<sumlocal[i][1]<<" "<<sumlocal[i][2]<<" "<<std::endl;
   }
 }
